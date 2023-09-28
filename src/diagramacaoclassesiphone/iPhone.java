7e6005e9eab75dc3c22ecae57b510c762520cf92
Akieu <andreoliveira886@yahoo.com.br>
2023-09-28 15:05:27 -0300

    up nos arquivos iPhone.class e .java

--- src/diagramacaoclassesiphone/iPhone.java
+++ src/diagramacaoclassesiphone/iPhone.java
@@ -3,78 +3,64 @@ package diagramacaoclassesiphone;
 public class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {    
     private String modelo;
     private String cor;
-    private int memoria;    
+    private int memoria;  
 
-    // Construtor da classe iPhone
+    
     public iPhone(String modelo, String cor, int memoria) {
         this.modelo = modelo;
         this.cor = cor;
-        this.memoria = memoria;
-        // Inicializar outros atributos se necessário
+        this.memoria = memoria;        
     }
 
-    public void imprimirInformacoes() {
-        // Imprimir as informações do objeto iPhone
+    public void imprimirInformacoes() {        
         System.out.println("Modelo: " + this.modelo);
         System.out.println("Cor: " + this.cor);
         System.out.println("Memória: " + this.memoria + " GB");
-    }
-    
+    }    
 
-    // Métodos da interface ReprodutorMusical
+    
     @Override
-    public void tocar() {
-        // Implementar a lógica para tocar uma música
+    public void tocar() {        
         System.out.println("tocar");
     }
 
     @Override
-    public void pausar() {
-        // Implementar a lógica para pausar uma música
+    public void pausar() {       
         System.out.println("pausar");
     }
 
     @Override
-    public void selecionarMusica(String musica) {
-        // Implementar a lógica para selecionar uma música
+    public void selecionarMusica(String musica) {       
         System.out.println("selecionarMusica: " + musica);
     }
-
-    // Métodos da interface AparelhoTelefonico
+    
     @Override
-    public void ligar() {
-        // Implementar a lógica para ligar para um número
+    public void ligar() {        
         System.out.println("ligar");
     }
 
     @Override
-    public void atender() {
-        // Implementar a lógica para atender uma chamada
+    public void atender() {        
         System.out.println("atender");
     }
 
     @Override
-    public void iniciarCorreioVoz() {
-        // Implementar a lógica para iniciar o correio de voz
+    public void iniciarCorreioVoz() {        
         System.out.println("iniciarCorreioVoz");
     }
-
-    // Métodos da interface NavegadorInternet
+    
     @Override
-    public void exibirPagina(String url) {
-        // Implementar a lógica para exibir uma página web
+    public void exibirPagina(String url) {        
         System.out.println("exibirPagina: " + url);
     }
 
     @Override
-    public void adicionarNovaAba() {
-        // Implementar a lógica para adicionar uma nova aba no navegador
+    public void adicionarNovaAba() {        
         System.out.println("adicionarNovaAba");
     }
 
     @Override
-    public void atualizarPagina() {
-        // Implementar a lógica para atualizar a página atual
+    public void atualizarPagina() {        
         System.out.println("atualizarPagina");
     }
 
